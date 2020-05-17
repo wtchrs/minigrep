1b2c5e721258b49a6df375aef6fa195955876b96
wtchrs <wtchr_@hotmail.com>
2020-05-17 18:20:54 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let cfg = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Failed to parse: {}.", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(cfg) {
+        eprintln!("Application failed: {}.", e);
+
+        process::exit(1);
+    };
+}
